312c0592fa721621a22378f91df24c4688c80521
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2021-10-13 14:13:08 -0400

    04 - Moving The Circle

--- raylib-vscode-template-main/axe_game.cpp
+++ raylib-vscode-template-main/axe_game.cpp
@@ -2,17 +2,36 @@
 
 int main()
 {
-    int width = 350;
-
+    // window dimensions
+    int width{350};
+    int height{200};
     InitWindow(width, 200, "Kevin-Brandon's Window!");
+
+    // circle coordinates
+    int circle_x{175};
+    int circle_y{100};
+
+    SetTargetFPS(60);
 
     while (WindowShouldClose() != true)
     {
         BeginDrawing();
         ClearBackground(WHITE);
 
-DrawCircle(175, 100, 25, BLUE);
+        // Game logic begins
+        DrawCircle(175, 100, 25, BLUE);
 
+        if (IsKeyDown(KEY_D))
+        {
+            circle_x = circle_x + 10;
+        }
+
+        if (IsKeyDown(KEY_A))
+        {
+            circle_x = circle_x - 10;
+        }
+
+        // Game logic ends
         EndDrawing();
     }
 } 
