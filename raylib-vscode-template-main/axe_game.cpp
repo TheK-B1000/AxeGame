bfaf17100b268ae2e420ce3918b709da10e47c43
Kevin-Brandon Joseph Corbett <k-b0514@hotmail.com>
2021-10-13 16:04:35 -0400

    08 - Collision Detection

--- raylib-vscode-template-main/axe_game.cpp
+++ raylib-vscode-template-main/axe_game.cpp
@@ -10,10 +10,24 @@ int main()
     // circle coordinates
     int circle_x{200};
     int circle_y{200};
+    int circle_radius{25};
+    // circle edges
+    int l_circle_x{circle_x - circle_radius};
+    int r_circle_x{circle_x + circle_radius};
+    int u_circle_y{circle_y - circle_radius};
+    int b_circle_y{circle_y + circle_radius};
+
 
     // axe coordinates
     int axe_x{400};
     int axe_y{0};
+    int axe_length{50};
+    // axe edges
+    int l_axe_x{axe_x};
+    int r_axe_x{axe_x + axe_length};
+    int u_axe_y{axe_y};
+    int b_axe_y{axe_y + axe_length};
+
 
     int direction{10};
 
@@ -25,18 +39,18 @@ int main()
         ClearBackground(WHITE);
 
         // Game logic begins
-        DrawCircle(175, 100, 25, BLUE);
-        DrawRectangle(axe_x, axe_y, 50, 50, RED);
+        DrawCircle(circle_x, circle_y, circle_radius, BLUE);
+        DrawRectangle(axe_x, axe_y, axe_length, axe_length, RED);
+
 
         // move the axe
          axe_y += direction;
-        if (axe_y > 450 || axe_y < 0)
+        if (axe_y > height || axe_y < 0)
         {
             direction = -direction;
         }
 
-
-        if (IsKeyDown(KEY_D) && circle_x < 350)
+        if (IsKeyDown(KEY_D) && circle_x < width)
         {
             circle_x += 10;
         }
